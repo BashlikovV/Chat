a3134033fe6448301c0b606beafc31091a4c7052
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-10 22:19:00 +0300

    ChatListItem.kt added (File with representation item of chat list).

---
+++ app/src/main/java/by/bashlikovv/chat/chat/list/item/ChatListItem.kt
@@ -0,0 +1,9 @@
+package by.bashlikovv.chat.chat.list.item
+
+import androidx.compose.runtime.Composable
+import by.bashlikovv.chat.chat.list.ChatListUiState
+
+@Composable
+fun ChatItem(data: ChatListUiState) {
+
+}
