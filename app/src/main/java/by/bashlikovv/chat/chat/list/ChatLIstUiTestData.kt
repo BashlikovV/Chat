633450068c4e7889f7eb948b9161fc0cb9904985
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-11 01:39:40 +0300

    ChatListUiTestData updated.

--- app/src/main/java/by/bashlikovv/chat/chat/list/ChatLIstUiTestData.kt
+++ app/src/main/java/by/bashlikovv/chat/chat/list/ChatLIstUiTestData.kt
@@ -3,19 +3,19 @@ package by.bashlikovv.chat.chat.list
 import by.bashlikovv.chat.R
 
 object ChatListUiTestData {
-    val chatListTestData = (0..9).map {
+    val chatListTestData = (0..90).map {
         ChatListUiState(
             image = listOf(R.drawable.test_face_man, R.drawable.test_face_woman).random(),
             name = "user name $it",
             displayedMessage = "displayed message $it",
-            time = getRandomTime()
+            time = getRandomTime(),
+            unreadMessagesCount = (0..20).random()
         )
     }
 
     fun getRandomTime(): String {
         val hh: String
         val mm: String
-        val ss: String
         var tmp = (0..12).random()
         hh = if (tmp < 10) {
             "0$tmp"
@@ -28,12 +28,6 @@ object ChatListUiTestData {
         } else {
             tmp.toString()
         }
-        tmp = (1..60).random()
-        ss = if (tmp < 10) {
-            "0$tmp"
-        } else {
-            tmp.toString()
-        }
-        return "$hh:$mm:$ss"
+        return "$hh:$mm"
     }
 }
