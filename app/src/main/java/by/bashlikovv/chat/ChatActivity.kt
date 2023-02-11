c7817f9af7bf7ece9d8ff7a3a3a6a7ff3eee44d4
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-11 17:23:28 +0300

    ChatActivity updated to new navigation component.

--- app/src/main/java/by/bashlikovv/chat/ChatActivity.kt
+++ app/src/main/java/by/bashlikovv/chat/ChatActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import by.bashlikovv.chat.chat.list.ChatsList
+import by.bashlikovv.chat.content.MainContent
 import by.bashlikovv.chat.ui.theme.ChatTheme
 
 class ChatActivity : ComponentActivity() {
@@ -18,21 +16,10 @@ class ChatActivity : ComponentActivity() {
         setContent {
             ChatTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    ChatsList()
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.primary) {
+                    MainContent()
                 }
             }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ChatTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-            ChatsList()
         }
     }
 }
