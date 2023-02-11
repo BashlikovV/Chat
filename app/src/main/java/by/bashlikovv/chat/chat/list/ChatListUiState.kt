aa36489153a2f697479554f8bce573d91dc93645
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-11 17:24:30 +0300

    ChatListUiState.kt updated.

--- app/src/main/java/by/bashlikovv/chat/chat/list/ChatListUiState.kt
+++ app/src/main/java/by/bashlikovv/chat/chat/list/ChatListUiState.kt
@@ -1,9 +1,11 @@
 package by.bashlikovv.chat.chat.list
+import by.bashlikovv.chat.chat.message.MessagesListUiSate
 
 data class ChatListUiState(
     val image: Int = 0,
     val name: String = "",
     val displayedMessage: String = "",
     val time: String = "",
-    val unreadMessagesCount: Int = 0
+    val unreadMessagesCount: Int = 0,
+    val messagesListUiSate: MessagesListUiSate = MessagesListUiSate()
 )
