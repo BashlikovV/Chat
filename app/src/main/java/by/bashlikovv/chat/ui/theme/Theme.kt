4376aae4f8f91ca6280dc8a4b870eec81998150c
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-11 17:27:26 +0300

    Theme values updated.

--- app/src/main/java/by/bashlikovv/chat/ui/theme/Theme.kt
+++ app/src/main/java/by/bashlikovv/chat/ui/theme/Theme.kt
@@ -7,15 +7,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = back,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    onSurface = surface
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    surface = Teal200
 
     /* Other default colors to override
     background = Color.White,
