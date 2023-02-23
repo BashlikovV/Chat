b6ae5d33b509c16be7ef5eba0a50ff7c123d17a2
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-02-23 10:09:15 +0300

    Parcelable support added.

--- app/src/main/java/by/bashlikovv/chat/struct/Message.kt
+++ app/src/main/java/by/bashlikovv/chat/struct/Message.kt
@@ -1,8 +1,12 @@
 package by.bashlikovv.chat.struct
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Message(
     val value: String = "",
     val user: User = User(),
     val time: String = "",
     val isRead: Boolean = false
-)
+) : Parcelable
--- app/src/main/java/by/bashlikovv/chat/struct/User.kt
+++ app/src/main/java/by/bashlikovv/chat/struct/User.kt
@@ -1,9 +1,12 @@
 package by.bashlikovv.chat.struct
 
+import android.os.Parcelable
 import androidx.annotation.DrawableRes
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class User(
     val userId: Long = 0,
     val userName: String = "",
     @DrawableRes val userImage: Int = 0
-)
+) : Parcelable
