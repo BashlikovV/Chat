6739cc00d342e8244bef2b1b3f03aa1c94b26059
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-01 22:21:28 +0300

    Login view model modified.

---
+++ app/src/main/java/by/bashlikovv/chat/login/LogInViewModel.kt
@@ -0,0 +1,139 @@
+package by.bashlikovv.chat.login
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.lifecycle.ViewModel
+import by.bashlikovv.chat.model.LogInUiState
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.util.*
+
+class LogInViewModel : ViewModel() {
+
+    private val _logInUiState = MutableStateFlow(LogInUiState())
+    val logInUiState = _logInUiState.asStateFlow()
+
+    fun onIdentifierChange(newValue: String) {
+        _logInUiState.update { it.copy(identifier = newValue) }
+    }
+
+    fun onPasswordChange(newValue: String) {
+        _logInUiState.update { it.copy(password = newValue) }
+    }
+
+    fun onUserNameChange(newValue: String) {
+        _logInUiState.update { it.copy(userName = newValue) }
+    }
+
+    fun applySuccess() {
+        _logInUiState.update { it.copy(isSuccess = true) }
+    }
+
+    fun onCheckInput(context: Context, haveAccount: Boolean) {
+        val email = _logInUiState.value.identifier
+        val password = _logInUiState.value.password
+        try {
+            if (haveAccount) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            _logInUiState.update {
+                                it.copy(isPasswordCorrect = true, isIdentifierCorrect = true, isSuccess = true)
+                            }
+                        } else {
+                            Toast
+                                .makeText(context, task.exception?.message ?: "Authentication error.", Toast.LENGTH_LONG)
+                                .show()
+                        }
+
+                    }
+            } else {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (!task.isSuccessful) {
+                            _logInUiState.update { it.copy(isPasswordCorrect = false, isIdentifierCorrect = false) }
+                            return@addOnCompleteListener
+                        }
+                        val user = FirebaseAuth.getInstance().currentUser
+                        val profileUpdates = UserProfileChangeRequest.Builder()
+                        profileUpdates.setDisplayName(_logInUiState.value.userName)
+                        profileUpdates.setPhotoUri(_logInUiState.value.userImageBitmap.userImageUri)
+                        user!!.updateProfile(profileUpdates.build())
+                        //
+                        saveUserToDatabase(_logInUiState.value.userImageBitmap.userImageUrl)
+                        //
+                        Toast
+                            .makeText(context, "Success. New user: ${task.result.user?.email}", Toast.LENGTH_LONG)
+                            .show()
+                        _logInUiState.update {
+                            it.copy(isPasswordCorrect = true, isIdentifierCorrect = true, isSuccess = true)
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Toast.makeText(context, exception.message, Toast.LENGTH_LONG).show()
+                    }
+            }
+        } catch (e: Exception) {
+            Toast.makeText(context, "Please, fill this fields", Toast.LENGTH_SHORT).show()
+            _logInUiState.update { it.copy(isPasswordCorrect = false, isIdentifierCorrect = false) }
+        }
+    }
+
+    fun onButtonClk(newValue: Boolean) {
+        _logInUiState.update { it.copy(isHaveAccount = newValue) }
+    }
+
+    fun selectImage(activity: ComponentActivity) {
+        val intent = Intent(Intent.ACTION_PICK).apply {
+            type = "image/*"
+        }
+        activity.startActivityForResult(intent, 0)
+    }
+
+    fun applyUserImageUri(newValue: Uri) {
+        val value = _logInUiState.value.userImageBitmap.copy(userImageUri = newValue)
+        _logInUiState.update { it.copy(userImageBitmap = value) }
+    }
+
+    fun applyUserBitmapImage(bitmap: ImageBitmap) {
+        val fileName = UUID.randomUUID()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$fileName")
+        val value = _logInUiState.value.userImageBitmap.copy(
+            userImageBitmap = bitmap.asAndroidBitmap(), userImageUrl = ref.downloadUrl.toString()
+        )
+        ref.putFile(_logInUiState.value.userImageBitmap.userImageUri)
+        _logInUiState.update { it.copy(userImageBitmap = value) }
+    }
+
+    private fun saveUserToDatabase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(
+            email = _logInUiState.value.identifier,
+            userName = _logInUiState.value.userName,
+            imageUri = profileImageUrl,
+            ref = ref
+        )
+
+//        ref.setValue(user)
+    }
+}
+
+class User(
+    val email: String,
+    val imageUri: String,
+    val userName: String,
+    val ref: DatabaseReference
+)
---
+++ app/src/main/java/by/bashlikovv/chat/model/LogInUiState.kt
@@ -0,0 +1,24 @@
+package by.bashlikovv.chat.model
+
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+data class LogInUiState(
+    val identifier: String = "",
+    val password: String = "",
+    val userName: String = "",
+    val userImageBitmap: UserImage = UserImage(),
+    val isIdentifierCorrect: Boolean = true,
+    val isPasswordCorrect: Boolean = true,
+    val isSuccess: Boolean = false,
+    val isHaveAccount: Boolean = false
+)
+
+@Parcelize
+data class UserImage(
+    val userImageBitmap: Bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888),
+    val userImageUri: Uri = Uri.EMPTY,
+    val userImageUrl: String = ""
+) : Parcelable
