46f6ca21b8088914ecb47ea29ac8a410e2c94cda
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-11 20:36:49 +0300

    Repositories.kt added.

--- app/src/main/java/by/bashlikovv/chat/Repositories.kt
+++ app/src/main/java/by/bashlikovv/chat/Repositories.kt
@@ -2,7 +2,13 @@ package by.bashlikovv.chat
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import by.bashlikovv.chat.model.accounts.AccountsRepository
+import by.bashlikovv.chat.model.accounts.SQLiteAccountsRepository
+import by.bashlikovv.chat.model.settings.MessengerSettings
+import by.bashlikovv.chat.model.settings.SharedPreferencesMessengerSettings
 import by.bashlikovv.chat.sqlite.MessengerSQLiteHelper
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 
 object Repositories {
 
@@ -10,5 +16,19 @@ object Repositories {
 
     private val database: SQLiteDatabase by lazy<SQLiteDatabase> {
         MessengerSQLiteHelper(applicationContext).writableDatabase
+    }
+
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+
+    private val appSettings: MessengerSettings by lazy {
+        SharedPreferencesMessengerSettings(applicationContext)
+    }
+
+    val accountsRepository: AccountsRepository by lazy {
+        SQLiteAccountsRepository(database, appSettings, ioDispatcher)
+    }
+
+    fun init(context: Context) {
+        applicationContext = context
     }
 }
