fadf61434134fcce8eb2b6485c4da604d0676f3b
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-12 22:24:46 +0300

    Functions for theming added.

--- app/src/main/java/by/bashlikovv/chat/model/settings/MessengerSettings.kt
+++ app/src/main/java/by/bashlikovv/chat/model/settings/MessengerSettings.kt
@@ -6,7 +6,12 @@ interface MessengerSettings {
 
     fun setCurrentAccountId(accountId: Long)
 
+    fun setCurrentAccountTheme(dark: Boolean)
+
+    fun getCurrentAccountTheme(): Boolean
+
     companion object {
         const val NO_ACCOUNT_ID = -1L
+        const val NO_ACCOUNT_THEME = true
     }
 }
