2b2d793afaf4bc2e55adb6bc395e1705db62bea5
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-26 19:24:24 +0300

    OkHttpSource added.

---
+++ app/src/main/java/by/bashlikovv/chat/sources/base/OkHttpSource.kt
@@ -0,0 +1,95 @@
+package by.bashlikovv.chat.sources.base
+
+import by.bashlikovv.chat.app.model.ParseBackendResponseException
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+/**
+ * Base class for all OkHttp sources.
+ */
+open class BaseOkHttpSource(
+    private val config: OkHttpConfig
+) {
+    val gson: Gson = config.gson
+    val client: OkHttpClient = config.client
+
+    private val contentType = "application/json; charset=utf-8".toMediaType()
+
+    suspend fun Call.suspendEnqueue(): Response {
+        return suspendCancellableCoroutine { continuation ->
+            continuation.invokeOnCancellation {
+                cancel()
+            }
+            enqueue(object : Callback {
+
+                override fun onFailure(call: Call, e: IOException) {
+                    continuation.resumeWithException(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        // well done
+                        continuation.resume(response)
+                    } else {
+                        handleErrorResponse(response, continuation)
+                    }
+                }
+            })
+        }
+    }
+
+    fun Request.Builder.endpoint(endpoint: String): Request.Builder {
+        url("${config.baseUrl}$endpoint")
+        return this
+    }
+
+    fun <T> T.toJsonRequestBody(): RequestBody {
+        val json = gson.toJson(this)
+        return json.toRequestBody(contentType)
+    }
+
+    fun <T> Response.parseJsonResponse(typeToken: TypeToken<T>): T {
+        try {
+            return gson.fromJson(this.body!!.string(), typeToken.type)
+        } catch (e: Exception) {
+            throw ParseBackendResponseException(e)
+        }
+    }
+
+    inline fun <reified T> Response.parseJsonResponse(): T {
+        try {
+            return gson.fromJson(this.body!!.string(), T::class.java)
+        } catch (e: Exception) {
+            throw ParseBackendResponseException(e)
+        }
+    }
+
+    private fun handleErrorResponse(
+        response: Response,
+        continuation: CancellableContinuation<Response>
+    ) {
+        val httpCode = response.code
+        try {
+            // parse error body:
+            // {
+            //   "error": "..."
+            // }
+            val map = gson.fromJson(response.body!!.string(), Map::class.java)
+            val message = map["error"].toString()
+            continuation.resumeWithException(Exception(message))
+        } catch (e: Exception) {
+            // failed to parse error body -> throw parse exception
+            val appException = ParseBackendResponseException(e)
+            continuation.resumeWithException(appException)
+        }
+    }
+
+}
