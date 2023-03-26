a104171328203e3489f166353dd09141fe9b8d55
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-26 19:25:07 +0300

    Field for store token added.

--- app/src/main/java/by/bashlikovv/chat/app/model/accounts/entities/Account.kt
+++ app/src/main/java/by/bashlikovv/chat/app/model/accounts/entities/Account.kt
@@ -4,8 +4,10 @@ data class Account(
     val id: Long,
     val username: String,
     val email: String,
+    val token: String,
     val createdAt: Long = UNKNOWN_CREATED_AT
 ) {
+
     companion object {
         const val UNKNOWN_CREATED_AT = 0L
     }
