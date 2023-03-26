1ab1272a584d9adc8590070d4dca94519d61219d
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-26 18:12:08 +0300

    Token column added.

--- app/src/main/assets/db_init.sql
+++ app/src/main/assets/db_init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "current_user" (
     "id"                INTEGER PRIMARY KEY,
     "email"             TEXT UNIQUE NOT NULL,
+    "token"             TEXT NOT NULL,
     "username"          TEXT NOT NULL,
     "password"          TEXT NOT NULL,
     "last_session_time" INTEGER NOT NULL
