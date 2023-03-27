f9ccc73a188fcdc2f6cafea57440823c35123195
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-27 12:48:12 +0300

    OkHttpMessagesSource added.

---
+++ app/src/main/java/by/bashlikovv/chat/sources/messages/OkHttpMessagesSource.kt
@@ -0,0 +1,11 @@
+package by.bashlikovv.chat.sources.messages
+
+import by.bashlikovv.chat.sources.base.BaseOkHttpSource
+import by.bashlikovv.chat.sources.base.OkHttpConfig
+
+class OkHttpMessagesSource(
+    config: OkHttpConfig
+) : BaseOkHttpSource(config) {
+
+
+}
