efc0e6e9cf8e5937c518101e77865917e3f3b3bb
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-28 11:58:45 +0300

    get user message implemented;

--- app/src/main/java/by/bashlikovv/chat/sources/users/OkHttpUsersSource.kt
+++ app/src/main/java/by/bashlikovv/chat/sources/users/OkHttpUsersSource.kt
@@ -4,6 +4,8 @@ import by.bashlikovv.chat.sources.users.entities.GetUsersResponseBody
 import by.bashlikovv.chat.sources.base.BaseOkHttpSource
 import by.bashlikovv.chat.sources.base.OkHttpConfig
 import by.bashlikovv.chat.sources.structs.User
+import by.bashlikovv.chat.sources.users.entities.GetUserRequestBody
+import by.bashlikovv.chat.sources.users.entities.GetUserResponseBody
 import okhttp3.Request
 
 class OkHttpUsersSource(
@@ -16,5 +18,15 @@ class OkHttpUsersSource(
             .build()
         val response = client.newCall(request).suspendEnqueue()
         return response.parseJsonResponse<GetUsersResponseBody>().users
+    }
+
+    suspend fun getUser(token: String): User {
+        val getUserRequestBody = GetUserRequestBody(token)
+        val request = Request.Builder()
+            .post(getUserRequestBody.toJsonRequestBody())
+            .endpoint("/get-user")
+            .build()
+        val response = client.newCall(request).suspendEnqueue()
+        return response.parseJsonResponse<GetUserResponseBody>().user
     }
 }
