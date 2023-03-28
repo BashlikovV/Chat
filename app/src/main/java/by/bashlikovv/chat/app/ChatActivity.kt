0a564cf8b73855834a31e77c5ce8857d033842d2
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-03-28 11:54:36 +0300

    Adding token into extra.

--- app/src/main/java/by/bashlikovv/chat/app/ChatActivity.kt
+++ app/src/main/java/by/bashlikovv/chat/app/ChatActivity.kt
@@ -1,11 +1,14 @@
 package by.bashlikovv.chat.app
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import by.bashlikovv.chat.Repositories
 import by.bashlikovv.chat.app.screens.chat.ChatView
@@ -15,19 +18,26 @@ import by.bashlikovv.chat.app.theme.MessengerTheme
 import by.bashlikovv.chat.app.utils.viewModelCreator
 
 class ChatActivity : ComponentActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val chatViewModel: ChatViewModel by viewModelCreator {
-            ChatViewModel()
+            ChatViewModel(Repositories.accountsRepository)
         }
         val darkTheme = intent.extras?.getBoolean(MessengerActivity.DARK_THEME)
         val data = intent.extras?.getParcelable<Chat>(MessengerActivity.CHAT)
+        val token = intent.extras?.getString(MessengerActivity.TOKEN) ?: ""
         if (data != null) {
             chatViewModel.applyChatData(data)
+            chatViewModel.applyMe(token)
         }
         Repositories.init(this)
 
         setContent {
+            LaunchedEffect(Unit) {
+                chatViewModel.getMessagesFromDb()
+            }
             MessengerTheme(darkTheme = darkTheme ?: true) {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     ChatView {
