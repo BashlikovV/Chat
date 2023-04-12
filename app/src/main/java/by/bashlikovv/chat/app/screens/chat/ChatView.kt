57cbb002c72b7568ab7be622a8625d3a43bd9ce1
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-12 15:07:01 +0300

    Code cleaned.

--- app/src/main/java/by/bashlikovv/chat/app/screens/chat/ChatView.kt
+++ app/src/main/java/by/bashlikovv/chat/app/screens/chat/ChatView.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
