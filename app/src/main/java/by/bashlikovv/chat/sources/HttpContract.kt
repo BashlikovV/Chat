c073f6149e4e7954932eb799efa887f5b489bd81
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-20 14:17:22 +0300

    HttpContract added.

---
+++ app/src/main/java/by/bashlikovv/chat/sources/HttpContract.kt
@@ -0,0 +1,22 @@
+package by.bashlikovv.chat.sources
+
+object HttpContract {
+
+    object UrlMethods {
+        const val SIGN_UP = "sign-up"
+        const val SIGN_IN = "sign-in"
+        const val ROOM_MESSAGES = "room-messages"
+        const val GET_ROOMS = "get-rooms"
+        const val ADD_ROOM = "add-room"
+        const val DELETE_ROOM = "delete-room"
+        const val ADD_MESSAGE = "add-message"
+        const val GET_USERS = "get-users"
+        const val GET_USERNAME = "get-username"
+        const val GET_ROOM = "get-room"
+        const val GET_USER = "get-user"
+        const val DELETE_MESSAGE = "delete-message"
+
+        const val GET_IMAGE = "get-image"
+        const val ADD_IMAGE = "add-image"
+    }
+}
