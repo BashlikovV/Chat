527085f5a41eb414a7465ee1f6d153275c2967dd
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-27 12:39:12 +0300

    Exceptions handling added.

--- app/src/main/java/by/bashlikovv/chat/sources/users/OkHttpUsersSource.kt
+++ app/src/main/java/by/bashlikovv/chat/sources/users/OkHttpUsersSource.kt
@@ -1,13 +1,13 @@
 package by.bashlikovv.chat.sources.users
 
 import by.bashlikovv.chat.sources.HttpContract
-import by.bashlikovv.chat.sources.users.entities.GetUsersResponseBody
 import by.bashlikovv.chat.sources.base.BaseOkHttpSource
 import by.bashlikovv.chat.sources.base.OkHttpConfig
 import by.bashlikovv.chat.sources.structs.User
 import by.bashlikovv.chat.sources.users.entities.GetUserRequestBody
 import by.bashlikovv.chat.sources.users.entities.GetUserResponseBody
 import by.bashlikovv.chat.sources.users.entities.GetUsersRequestBody
+import by.bashlikovv.chat.sources.users.entities.GetUsersResponseBody
 import okhttp3.Request
 
 class OkHttpUsersSource(
@@ -15,24 +15,34 @@ class OkHttpUsersSource(
 ) : BaseOkHttpSource(config) {
 
     suspend fun getAllUsers(token: String): List<User> {
-        val getUsersRequestBody = GetUsersRequestBody(
-            token = token
-        )
-        val request = Request.Builder()
-            .post(getUsersRequestBody.toJsonRequestBody())
-            .endpoint("/${HttpContract.UrlMethods.GET_USERS}")
-            .build()
-        val response = client.newCall(request).suspendEnqueue()
-        return response.parseJsonResponse<GetUsersResponseBody>().users
+        return try {
+            val getUsersRequestBody = GetUsersRequestBody(
+                token = token
+            )
+            val request = Request.Builder()
+                .post(getUsersRequestBody.toJsonRequestBody())
+                .endpoint("/${HttpContract.UrlMethods.GET_USERS}")
+                .build()
+            val response = client.newCall(request).suspendEnqueue()
+            response.parseJsonResponse<GetUsersResponseBody>().users
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return listOf(User())
+        }
     }
 
     suspend fun getUser(token: String): User {
-        val getUserRequestBody = GetUserRequestBody(token)
-        val request = Request.Builder()
-            .post(getUserRequestBody.toJsonRequestBody())
-            .endpoint("/${HttpContract.UrlMethods.GET_USER}")
-            .build()
-        val response = client.newCall(request).suspendEnqueue()
-        return response.parseJsonResponse<GetUserResponseBody>().user
+        return try {
+            val getUserRequestBody = GetUserRequestBody(token)
+            val request = Request.Builder()
+                .post(getUserRequestBody.toJsonRequestBody())
+                .endpoint("/${HttpContract.UrlMethods.GET_USER}")
+                .build()
+            val response = client.newCall(request).suspendEnqueue()
+            response.parseJsonResponse<GetUserResponseBody>().user
+        } catch (e: Exception) {
+            e.printStackTrace()
+            User()
+        }
     }
 }
