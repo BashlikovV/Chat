088809cad85dc998478916783173d6e572c565a9
bashlikovV <volodia413314@gmail.com>
2023-05-12 22:11:41 +0300

    Sign in, get username and sign up methods added.

--- app/src/main/java/by/bashlikovv/chat/app/model/users/UsersRepository.kt
+++ app/src/main/java/by/bashlikovv/chat/app/model/users/UsersRepository.kt
@@ -1,5 +1,6 @@
 package by.bashlikovv.chat.app.model.users
 
+import android.graphics.Bitmap
 import by.bashlikovv.chat.app.screens.login.UserImage
 import by.bashlikovv.chat.sources.structs.User
 
@@ -12,4 +13,10 @@ interface UsersRepository {
     suspend fun getUser(token: String): User
 
     suspend fun updateUsername(token: String, newName: String)
+
+    suspend fun signIn(email: String, password: String): String
+
+    suspend fun getUsername(token: String): String
+
+    suspend fun signUp(email: String, password: String, username: String, image: Bitmap)
 }
