fce478b2ee9f91237aed1f248fa9f9ea717c62e7
bashlikovV <volodia413314@gmail.com>
2023-05-12 15:56:55 +0300

    Get user method added.

--- app/src/main/java/by/bashlikovv/chat/app/model/users/UsersRepository.kt
+++ app/src/main/java/by/bashlikovv/chat/app/model/users/UsersRepository.kt
@@ -1,10 +1,13 @@
 package by.bashlikovv.chat.app.model.users
 
 import by.bashlikovv.chat.app.screens.login.UserImage
+import by.bashlikovv.chat.sources.structs.User
 
 interface UsersRepository {
 
     suspend fun getUserImage(uri: String): UserImage
 
-    suspend fun getUsers(token: String): List<by.bashlikovv.chat.sources.structs.User>
+    suspend fun getUsers(token: String): List<User>
+
+    suspend fun getUser(token: String): User
 }
