a157b556d0715e8ee276a895c8e72365ddf3e618
bashlikovV <volodia413314@gmail.com>
2023-05-22 23:04:43 +0300

    Theming changed.

--- app/src/main/java/by/bashlikovv/chat/app/theme/Theme.kt
+++ app/src/main/java/by/bashlikovv/chat/app/theme/Theme.kt
@@ -3,29 +3,33 @@ package by.bashlikovv.chat.app.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = PrimaryDark,
-    secondary = SecondaryDark,
-    primaryVariant = PrimaryLight,
-    background = Background
+val darkColorsPalette = darkColors(
+    background = Color(0xFF131313),
+    primary = Color(0xFF1E1E1E),
+    secondary = Color(0xFF1C6D78),
+    onSurface = Color(0xFFFFFFFF),
+    surface = Color(0xFF9D9C9C),
+    onError = Color(0xFFE39801)
 )
 
-private val LightColorPalette = lightColors(
-    primary = SecondaryLight,
-    secondary = PrimaryLight,
-    primaryVariant = SecondaryLight,
-    background = PrimaryLight
+val lightColorsPalette = darkColors(
+    background = Color(0xFFFFFFFF),
+    primary = Color(0xFFF8F8F8),
+    secondary = Color(0xFF92D4DD),
+    onSurface = Color(0xFF000000),
+    surface = Color(0xFF5E5E5E),
+    onError = Color(0xFFE39801)
 )
 
 @Composable
 fun MessengerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        darkColorsPalette
     } else {
-        LightColorPalette
+        lightColorsPalette
     }
 
     MaterialTheme(
