6a8ffacebf5d699e0d000b1c72423641ff102f79
bashlikovV <volodia413314@gmail.com>
2023-05-23 21:06:14 +0300

    SettingsView added.

---
+++ app/src/main/java/by/bashlikovv/chat/app/screens/messenger/settings/SettingsView.kt
@@ -0,0 +1,9 @@
+package by.bashlikovv.chat.app.screens.messenger.settings
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsView() {
+    Text(text = "Settings")
+}
