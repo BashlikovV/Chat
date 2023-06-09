2728917dc1d32c2a2f4a045801f3857f3b8f06b9
bashlikovV <volodia413314@gmail.com>
2023-06-09 14:19:53 +0300

    DensityUtils.kt added.

---
+++ app/src/main/java/by/bashlikovv/chat/app/views/bottombar/Utils/DensityUtils.kt
@@ -0,0 +1,8 @@
+package by.bashlikovv.chat.app.views.bottombar.Utils
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+
+@Stable
+fun Dp.toPxf(density: Density): Float = with(density) { this@toPxf.toPx() }
