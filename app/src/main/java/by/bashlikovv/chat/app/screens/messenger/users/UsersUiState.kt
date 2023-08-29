15b3805265f54e65fd64c9fd12bbfa1e98d74c16
bashlikovvv <volodia413314@gmail.com>
2023-08-29 23:23:19 +0300

    removed.

--- app/src/main/java/by/bashlikovv/chat/app/screens/messenger/users/UsersUiState.kt
+++
@@ -1,5 +0,0 @@
-package by.bashlikovv.chat.app.screens.messenger.users
-
-import by.bashlikovv.chat.app.struct.Chat
-
-data class UsersUiState(val chats: List<Chat> = listOf())
--- app/src/main/java/by/bashlikovv/chat/sources/users/entities/GetUsername.kt
+++
@@ -1,9 +0,0 @@
-package by.bashlikovv.chat.sources.users.entities
-
-data class GetUsernameRequestBody(
-    val token: String
-)
-
-data class GetUsernameResponseBody(
-    val username: String
-)
