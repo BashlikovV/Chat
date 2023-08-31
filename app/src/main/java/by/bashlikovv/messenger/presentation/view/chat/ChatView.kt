b1103b2b8e7b09978e3fbb3b056d739127baf579
bashlikovvv <volodia413314@gmail.com>
2023-08-31 14:29:26 +0300

    Image type changed.

--- app/src/main/java/by/bashlikovv/messenger/presentation/view/chat/ChatView.kt
+++ app/src/main/java/by/bashlikovv/messenger/presentation/view/chat/ChatView.kt
@@ -2,12 +2,20 @@ package by.bashlikovv.messenger.presentation.view.chat
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
@@ -18,27 +26,38 @@ import androidx.compose.material.Text
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.DisposableEffectScope
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.*
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import by.bashlikovv.messenger.R
 import by.bashlikovv.messenger.domain.model.Message
 import by.bashlikovv.messenger.presentation.view.theme.MessageShape
 import by.bashlikovv.messenger.presentation.viewmodel.ChatViewModel
 import by.bashlikovv.messenger.utils.buildTime
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -268,6 +287,7 @@ private fun MessageView(
 
 @Composable
 private fun MessageImageView(message: Message) {
+
     var scaleState by remember { mutableFloatStateOf(1f) }
     var translationState by remember { mutableStateOf(Offset(0f, 0f)) }
     val state = rememberTransformableState { zoomChange, panChange, _ ->
@@ -275,8 +295,11 @@ private fun MessageImageView(message: Message) {
         translationState += panChange
     }
 
-    Image(
-        bitmap = message.imageBitmap.asImageBitmap(),
+    AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(message.imageBitmap)
+            .crossfade(true)
+            .build(),
         contentDescription = message.value,
         modifier = Modifier
             .transformable(state)
